0dab003e1d8d4ab246125755e48bae49098b9bee
DAVE2456 <davehunegnaw@gmail.com>
2022-06-08 01:53:47 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,17 @@
-#include <main.h>                                                                                                                                
-/**                                                                                                                                              
- * main -Starting point                                                                                                                          
- *                                                                                                                                               
- * Description: Function prototype                                                                                                               
- * Return : 0 means a ssucces                                                                                                                    
- */                                                                                                                                              
-int main(void)                                                                                                                                   
-{                                                                                                           char str[8] = "_putchar";                                                                                                                                                                              int i = 0;                                                                                                                            
-        while (i < 8)                                                                
-        {                                                                                                                                        
-                _putchar(str[i]);                                                                                                                
-                ++i;                                                                                                                             
-        }                                                                                                   _putchar('\n');                                                                                    
-        return (0);                                                             
+#include "main.h"
+/**
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar writes character c to std
+ * @c:parameter to compare
+ * Return: 1 or 0
+ */
 int _putchar(char c);
+#endif
