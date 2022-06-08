de863bb01179f29249d3c1c90d811cacad5bdf71
DAVE2456 <davehunegnaw@gmail.com>
2022-06-08 04:28:24 -0700

    task 2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,13 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-/**
- * _putchar writes character c to std
- * @c:parameter to compare
- * Return: 1 or 0
- */
-int _putchar(char c);
-/**
- * void print_alphabet(void) - print alphabet
- *
- */
+int _putchar(int c);
 void print_alphabet(void);
-/**
- * void print_alphabet_x10(void) - print alphabet
- *
- */
 void print_alphabet_x10(void);
-#endif
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
