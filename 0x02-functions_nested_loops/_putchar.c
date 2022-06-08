bc4035eb9dcc1636ba9aefecaa676b42bc91de25
DAVE2456 <davehunegnaw@gmail.com>
2022-06-08 02:42:27 -0700

    task 1

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -11,3 +11,16 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void);
+{
+	return (write(1, &c, 1));
+}
