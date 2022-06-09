54c0aa754e912387efe6febd95f8b9e6e487f3f2
DAVE2456 <davehunegnaw@gmail.com>
2022-06-09 05:12:39 -0700

    task1

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -9,3 +12,5 @@ void print_diagonal(int n);
 void print_square(int size);
 oid print_triangle(int size);
 void print_number(int n);
+
+#endif
