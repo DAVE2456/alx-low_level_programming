4a395580f5eff3146fb0aaf8e8b1beb3c504de8a
DAVE2456 <davehunegnaw@gmail.com>
2022-06-09 10:23:27 -0700

    task 2

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,7 +7,3 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-FizzBuzz
-void print_triangle(int size);
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-prime_factor.c -o 100-prime_factor -lm
-void print_number(int n);
