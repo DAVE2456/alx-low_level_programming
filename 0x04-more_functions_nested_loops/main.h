7a89bba6dca84beb8756667075b0ab34cff5156b
DAVE2456 <davehunegnaw@gmail.com>
2022-06-09 10:18:05 -0700

    task 2

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,13 @@
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+FizzBuzz
+void print_triangle(int size);
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-prime_factor.c -o 100-prime_factor -lm
+void print_number(int n);
