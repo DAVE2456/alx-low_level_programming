878a303d3bc415d40c6bb46aad5365afdefe2011
DAVE2456 <davehunegnaw@gmail.com>
2022-06-13 04:47:32 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * reset_to_98 _takes a pointer to a parameter and updates its value.
+ * reset_to_98 - takes a pointer to a parameter and updates its value.
  * @n: input integer.
  * Return: no return.
  */
