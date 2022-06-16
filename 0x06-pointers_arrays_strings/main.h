344446e679cc3f9b45129c8538e47cc3d751ccc0
DAVE2456 <davehunegnaw@gmail.com>
2022-06-16 06:36:44 -0700

    task 102

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,7 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+char *leet(char *);
+char *cap_string(char *);
 
 #endif
