0d56e83acac4193f9ef185ad360eb0d5602227a0
DAVE2456 <davehunegnaw@gmail.com>
2022-06-22 06:03:24 -0700

    task 0

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,12 +5,12 @@
  * @n: input number.
  * Return: factorial of the number.
  */
-int factorial (int n)
+int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
-		return(1);
+		return (1);
 	else
 		return (n * factorial(n - 1));
 }
