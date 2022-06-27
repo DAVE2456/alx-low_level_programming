53236a388f521a56c4c65dc7e000cee6c5b8405f
DAVE2456 <davehunegnaw@gmail.com>
2022-06-27 05:48:49 -0700

    task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,7 @@
 #includ MAIN_H
 #define MAIN_H
 
-char *creat_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **allco_grid(int width, int height);
