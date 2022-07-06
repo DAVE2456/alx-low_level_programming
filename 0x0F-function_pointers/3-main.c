4ba332a3dc4a1b7368b724f3db9e47f10c71f32f
DAVE2456 <davehunegnaw@gmail.com>
2022-07-06 04:20:36 -0700

    0-print_name.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,40 +1,40 @@
-i#include "3-calc.h"
+#include "3-calc.h"
 
 /**
- *  * main - check the code for alx School students.
- *   * @argc: argument count.
- *    * @argv: argument vector.
- *     *
- *      * Return: Always 0.
- *       */
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-		int a, b;
-			int (*operation)(int, int);
+	int a, b;
+	int (*operation)(int, int);
 
-				if (argc != 4)
-						{
-									printf("Error\n");
-											exit(98);
-												}
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-					if (argv[2][1])
-							{
-										printf("Error\n");
-												exit(99);
-													}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-						operation = get_op_func(argv[2]);
+	operation = get_op_func(argv[2]);
 
-							if (operation == NULL)
-									{
-												printf("Error\n");
-														exit(99);
-															}
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-								a = atoi(argv[1]);
-									b = atoi(argv[3]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-										printf("%d\n", operation(a, b));
-											return (0);
+	printf("%d\n", operation(a, b));
+	return (0);
 }
