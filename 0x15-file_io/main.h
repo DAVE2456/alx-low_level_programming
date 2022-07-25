e9ea1228ac03a45e5da2534e2abfcf3b88d9b4e5
DAVE2456 <davehunegnaw@gmail.com>
2022-07-25 07:39:55 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,15 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _MAIN_H
+#define _MAIN_H
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
+#include <unistd.h>
+
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endi
+#en
