832baa1fa61f6f2470c30a82a924368b30cc48b6
DAVE2456 <davehunegnaw@gmail.com>
2023-01-04 22:58:39 +0300

    initial commit

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,25 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/*
+ * File: search_algos.h
+ * Desc: This header file contains all the prototypes required for
+ * the 0x1E-search_algorithms project.
+ *
+ * Author: Gizachew Bayness (Elec Crazy)
+ * Date Created: Oct 31, 2022
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif
